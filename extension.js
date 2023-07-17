5e372363f346125a1b72069c6608663e03f844ea
unlomtrois <vadim.kuz02@gmail.com>
2023-07-17 23:14:06 +0300

    add status bar button, add basic settings

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -8,29 +8,63 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+async function activate(context) {
+  // status bar
+  const statusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    100
+  );
+  statusBarItem.text = "🐯Run ck3tiger🐯";
+  statusBarItem.tooltip = "Run ck3tiger (tooltip)";
+  statusBarItem.command = "ck3tiger-for-vscode.runCk3tiger";
+  statusBarItem.show();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ck3tiger-for-vscode" is now active!');
+  const configuration = vscode.workspace.getConfiguration("ck3tiger");
+  await vscode.window
+    .showInputBox({ prompt: "Set path to your ck3tiger" })
+    .then((ck3tiger_path) => {
+      if (ck3tiger_path) {
+        configuration.update(
+          "ck3tiger.path",
+          ck3tiger_path,
+          vscode.ConfigurationTarget.Global
+        );
+      }
+    });
+  await vscode.window
+    .showInputBox({ prompt: "Set path to your vanilla ck3" })
+    .then((vanilla_path) => {
+      if (vanilla_path) {
+        configuration.update(
+          "vanilla.path",
+          vanilla_path,
+          vscode.ConfigurationTarget.Global
+        );
+      }
+    });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ck3tiger-for-vscode.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+  let run_ck3tiger_command = vscode.commands.registerCommand(
+    "ck3tiger-for-vscode.runCk3tiger",
+    function () {
+      vscode.window.showInformationMessage("🐅🐅🐅RAWR! ANGRY TIGER!🐯🐯🐯");
+    }
+  );
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ck3tiger for vscode!');
-	});
-
-	context.subscriptions.push(disposable);
+  let hello = vscode.commands.registerCommand(
+    "ck3tiger-for-vscode.helloWorld",
+    function () {
+      vscode.window.showInformationMessage(
+        "Hello World from ck3tiger for vscode!"
+      );
+    }
+  );
+  context.subscriptions.push(statusBarItem, hello, run_ck3tiger_command);
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
